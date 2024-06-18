935feda0428e0ad961c7a2f3a524fbb5b5ba56bd
PatrykPytel <127932899+PatrykPytel@users.noreply.github.com>
2024-06-18 23:43:38 +0200

    :##33430yu2igubiugb

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-const CVC = document.getElementsByClassName("CVC-code");
-const element = CVC.getElementsByTagName("p");
-const inputcvc = document.getElementById("cvc");
-element = inputcvc;
+function verify(reg, x) { 
+    const y = document.getElementById("backcard");
+    if (!(reg.test(x))) { 
+        y.innerHTML = "KURWA";
+    }
+}
+function change0() { 
+    const re = new RegExp("^[0-9]{3}$"); 
+    const y = document.getElementById("cardname");
+    const x = document.getElementById("name");
+    y.innerHTML = x.value;
+    verify(re,x.value);
+}
+function change1() { 
+    const re = new RegExp("^[0-9]{3}$"); 
+    const y = document.getElementById("cardnumbers");
+    const x = document.getElementById("numbers");
+    y.innerHTML = x.value;
+    verify(re,x.value);
+}
+function change2() { 
+    const re = new RegExp("^[0-9]{3}$"); 
+    const y = document.getElementById("month");
+    const x = document.getElementById("mm");
+    y.innerHTML = x.value;
+    verify(re,x.value);
+}
+function change3() { 
+    const re = new RegExp("^[0-9]{3}$"); 
+    const y = document.getElementById("year");
+    const x = document.getElementById("yy");
+    y.innerHTML = x.value;
+    verify(re,x.value);
+}
+function change4() {
+    const re = new RegExp("^[0-9]{3}$"); 
+    const y = document.getElementById("backcard");
+    const x = document.getElementById("cvc");
+    y.innerHTML = x.value;
+    verify(re,x.value);
+
+
+}
